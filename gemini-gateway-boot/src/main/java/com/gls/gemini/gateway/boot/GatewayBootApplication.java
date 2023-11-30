8b18563ce2545493c5d65f31fe63bd2518432893
李智勇 <george510257@gmail.com>
2023-11-30 20:08:42 +0800

    init

---
+++ gemini-gateway-boot/src/main/java/com/gls/gemini/gateway/boot/GatewayBootApplication.java
@@ -0,0 +1,11 @@
+package com.gls.gemini.gateway.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayBootApplication.class, args);
+    }
+}
