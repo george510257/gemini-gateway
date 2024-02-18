7f98ece4a6befe394606858701fb318717b4a434
李智勇 <george510257@gmail.com>
2024-02-18 23:40:21 +0800

    init gemini gateway

--- gemini-gateway-boot/src/main/java/com/gls/gemini/gateway/boot/listener/OpenApiListener.java
+++ gemini-gateway-boot/src/main/java/com/gls/gemini/gateway/boot/listener/OpenApiListener.java
@@ -1,7 +1,8 @@
 package com.gls.gemini.gateway.boot.listener;
 
+import cn.hutool.core.util.StrUtil;
 import jakarta.annotation.Resource;
-import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
@@ -9,7 +10,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -27,11 +27,6 @@ public class OpenApiListener implements ApplicationListener<RefreshRoutesEvent> 
      */
     @Resource
     private SwaggerUiConfigProperties swaggerUiConfigProperties;
-    /**
-     * 聚合多个服务的接口文档
-     */
-    @Resource
-    private List<GroupedOpenApi> groupedOpenApis;
 
     /**
      * 刷新路由事件
@@ -40,20 +35,15 @@ public class OpenApiListener implements ApplicationListener<RefreshRoutesEvent> 
      */
     @Override
     public void onApplicationEvent(RefreshRoutesEvent event) {
-        // 清空接口文档
-        groupedOpenApis.clear();
-        // 清空接口文档url
-        Set<SwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
+        Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
         // 获取路由定义
         routeDefinitionLocator.getRouteDefinitions()
                 .map(routeDefinition -> routeDefinition.getUri().getHost())
-                .filter(host -> host != null && !host.isBlank())
+                .filter(StrUtil::isNotBlank)
                 .distinct()
                 .subscribe(host -> {
-                    // 聚合接口文档
-                    groupedOpenApis.add(GroupedOpenApi.builder().pathsToMatch("/" + host + "/**").group(host).build());
                     // 聚合接口文档url
-                    urls.add(new SwaggerUiConfigProperties.SwaggerUrl(host, "/" + host + "/v3/api-docs", host));
+                    urls.add(new AbstractSwaggerUiConfigProperties.SwaggerUrl(host, "/" + host + "/v3/api-docs", host));
                 });
         // 设置接口文档url
         swaggerUiConfigProperties.setUrls(urls);
