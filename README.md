dd9d0713f4796f7ab5f8f14f4907cf5fd5f39de7
李智勇 <george510257@gmail.com>
2024-02-19 22:34:41 +0800

    init :: 通过代码生成器生成

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# gemini-gateway
+- 项目名称：gemini-gateway
+- 项目描述：网关服务
 
-## 项目介绍
+```lua
 
-网关服务
+```
---
+++ db/gateway.sql
@@ -0,0 +1,11 @@
+-- ----------------------------
+--  数据库初始化脚本
+-- ----------------------------
+-- 1. 创建数据库
+drop database if exists gemini_gateway;
+
+create database gemini_gateway default character set utf8mb4 collate utf8mb4_general_ci;
+
+use gemini_gateway;
+
+set names utf8mb4;
